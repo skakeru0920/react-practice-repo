18b4d741bab22022424c6d5c9029cc09632c793e
Kakeru Sano <skakeru0920@gmail.com>
2024-12-23 11:44:35 +0900

    first routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Notes from "./routes/Notes.jsx";
+import Home from "./routes/Home.jsx";
+import NotFound from "./routes/NotFound.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<BrowserRouter>
+		<Routes>
+			<Route index element={<Home />} />
+			<Route path="/notes" element={<Notes />} />
+			<Route path="/*" element={<NotFound />} />
+		</Routes>
+	</BrowserRouter>
+);
